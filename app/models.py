7a08a4910338888c63ad747971fd5119ceea8ec8
Erneris <ernestassin1@gmail.com>
2022-06-29 14:26:06 +0300

    nu

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.CharField(max_length = 200)
+    user = models.CharField(max_length = 15)
+    category = models.CharField(max_length = 20)
+    date = models.DateField()
--- app/views.py
+++ app/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django import forms
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import post
+
 
 def index(request):
     return render(request, "index.html")
